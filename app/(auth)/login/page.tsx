d266791c7ca3a6f609d319104a0fda6e2a7cf60f
Nitesh Vishwakarma <nitesh.vishwakarmangts@gmail.com>
2026-01-10 19:40:34 +0530

    updated

--- app/(auth)/login/page.tsx
+++
@@ -1,8 +0,0 @@
-import LoginPage from '@/components/auth/login/page'
-import React from 'react'
-
-export default function Login(){
-  return (
-    <div><LoginPage /></div>
-  )
-}
--- app/(auth)/otp-verify/page.tsx
+++
@@ -1,9 +0,0 @@
-import OtpVerifyPage from '@/components/auth/otp-verify/page'
-import React from 'react'
-
-export default function OtpVerify (){
-  return (
-    <div><OtpVerifyPage /></div>
-  )
-}
-
