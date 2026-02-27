790a7181d28f68f3e0333ebf82fd78a41994e248
Nitesh Vishwakarma <nitesh.vishwakarma@ngtsindore.in>
2026-02-27 11:31:29 +0530

    updated code with build

--- app/(auth)/otp-verify/page.tsx
+++ app/(auth)/otp-verify/page.tsx
@@ -1,9 +1,11 @@
-import OtpVerifyPage from '@/components/auth/otp-verify/page'
-import React from 'react'
+import React, { Suspense } from "react";
+import OtpVerifyPage from "@/components/auth/otp-verify/page";
 
-export default function OtpVerify (){
+export default function OtpVerify() {
   return (
-    <div><OtpVerifyPage /></div>
-  )
+    <Suspense fallback={null}>
+      <OtpVerifyPage />
+    </Suspense>
+  );
 }
 
